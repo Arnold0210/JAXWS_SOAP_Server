b66e31d93af7ae3393b640e3154d8e1a3663d278
Arnold Herrera <arnoldjulianherreraquiones@gmail.com>
2020-08-02 02:26:37 -0500

    Se añade versionamiento al proyecto mediante maven y git

--- src/main/java/co/com/facturas/CalculadoraPublish.java
+++ src/main/java/co/com/facturas/CalculadoraPublish.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 import co.com.facturas.soap.CalculadoraImpl;
 
 public class CalculadoraPublish {
-
+	
 	public static void main(String[] args) {
 //		
 		Endpoint.publish("http://localhost:8081/CalculadoraSOAP", new CalculadoraImpl());
