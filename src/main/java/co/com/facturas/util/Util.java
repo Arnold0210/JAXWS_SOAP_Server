f79eb4b89748feb239edd8d2bddf33884b159627
Arnold <arnoldjulianherreraquiones@gmail.com>
2020-08-18 01:03:33 -0500

    Se ajusta auditoria con log4j y archivo properties y se limpia repositorio

--- src/main/java/co/com/facturas/util/Util.java
+++ src/main/java/co/com/facturas/util/Util.java
@@ -1,14 +1,20 @@
 package co.com.facturas.util;
 
 import org.apache.log4j.Logger;
+
 //import org.apache.log4j.xml.DOMConfigurator;
 
 public class Util {
-	private static Logger log = Logger.getLogger(Util.class);
 
-	@SuppressWarnings("rawtypes")
+	private Util() {
+		throw new IllegalStateException("Utiility Class");
+	}
+
+	@SuppressWarnings({ "rawtypes" })
 	public static void registarInfo(Class clase, TipoLog tipo, Object message) {
 		// DOMConfigurator.configure("src/main/resources/log4j.properties");
+		Logger log = Logger.getLogger(clase);
+
 		switch (tipo) {
 		case DEBUG:
 			log.debug(message);
